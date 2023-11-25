55d4fdb197b1977b1cef20b5ed3f3dbee3bbfabb
TaibkKurbanaliev <taibkurbanaliev@gmail.com>
2023-11-25 20:42:12 +0300

    Добавьте файлы проекта.

---
+++ PCBuilder.DAL/Interfaces/IBaseRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PCBuilder.DAL.Interfaces
+{
+    public interface IBaseRepository<T>
+    {
+        Task<bool> Create(T entity);
+        Task<T> Get(int id);
+        Task<IEnumerable<T>> GetAll();
+        Task<bool> Delete(int id);
+        Task<bool> Edit(T entity);
+        Task<bool> Save();
+    }
+}
---
+++ PCBuilder.DAL/Repositories/PCRepository.cs
@@ -0,0 +1,43 @@
+﻿using PCBuilder.DAL.Interfaces;
+using PCBuilder.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PCBuilder.DAL.Repositories
+{
+    public class PCRepository : IPCRepository
+    {
+        public Task<bool> Create(PC entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Edit(PC entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<PC> Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<PC>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Save()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
